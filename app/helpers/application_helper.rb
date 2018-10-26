8a31e846144b40420bd46b458ef7fd78eed0d68e
Chris Want <cjwant@gmail.com>
2018-10-25 22:01:03 -0600

    Basically a minimal viable product if you have really low standards.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,32 @@
 module ApplicationHelper
+  def search_tab_label(searcher, active_searcher: nil, count: nil)
+    label = t("searchers.names.#{searcher}")
+    label += " (#{count})" if count
+    klass = 'nav-link'
+    is_active = (searcher == active_searcher)
+    klass += ' active' if is_active
+    content_tag(:li, class: 'nav-item') do
+      content_tag(:a, label,
+                  href: "##{searcher}",
+                  id: "#{searcher}-tab",
+                  'data-toggle': 'tab',
+                  class: klass,
+                  'aria-controls': searcher,
+                  'aria-selected': is_active.to_s)
+    end
+  end
+
+  def search_tab_panel(searcher, active_searcher: nil)
+    klass = 'tab-pane fade search-results'
+    is_active = (searcher == active_searcher)
+    klass += ' show active' if is_active
+    content_tag(:div, "Content #{searcher}",
+                id: searcher.to_s,
+                role: 'tabpanel',
+                class: klass,
+                'aria-labelledby': "#{searcher}-tab",
+                'data-tab-id': "#{searcher}-tab",
+                'data-label': t(searcher),
+                'data-base-url': searcher_path(searcher, format: :json))
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :searchers, only: [:index] do
-    collection do
-      get 'slack'
-      get 'mediawiki'
-    end
-  end
+  root to: 'searchers#index'
 
+  resources :searchers, only: [:index, :show]
 end
