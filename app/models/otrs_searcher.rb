ba2e6b7e42cfd6ee490faa5e51d5d975e824998a
Chris Want <cjwant@gmail.com>
2019-03-06 15:34:54 -0700

    Support for OTRS (5, probably 6).

---
+++ app/models/otrs_searcher.rb
@@ -0,0 +1,83 @@
+class OtrsSearcher < Searcher
+
+  def initialize(config)
+    @base_url = config[:base_url]
+    @username = config[:username]
+    @password = config[:password]
+  end
+
+  def reify
+    return results if results.any?
+
+    return results unless criteria[:term]
+
+    response = RestClient::Request.execute(method: :get,
+                                           url: search_endpoint,
+                                           payload: search_payload,
+                                           headers: {content_type: :json,
+                                                     accept: :json})
+    @results = JSON.parse(response)
+    fetch_tickets
+  end
+
+  def iteratable_results
+    results['tickets']
+  end
+
+  def total_count
+    reify
+    results['TicketID']&.count || 0
+  end
+
+  def count
+    reify
+    results['tickets']&.count || 0
+  end
+
+  def hit_url(hit)
+    "#{@base_url}/otrs/index.pl?Action=AgentTicketZoom;TicketID=#{hit['TicketID']}"
+  end
+
+  private
+
+  def search_endpoint
+    "#{@base_url}/otrs/nph-genericinterface.pl/Webservice/"\
+    'GenericTicketConnectorREST/Ticket'
+  end
+
+  def search_payload
+    {'UserLogin' => @username,
+     'Password' => @password,
+     'Fulltext' => criteria[:term]
+    }.to_json
+  end
+
+  def ticket_list_endpoint
+    "#{@base_url}/otrs/nph-genericinterface.pl/Webservice/"\
+    'GenericTicketConnectorREST/TicketList'
+  end
+
+  def ticket_list_payload(tickets)
+    {'UserLogin' => @username,
+     'Password' => @password,
+     'TicketID' => tickets
+    }.to_json
+  end
+
+  def fetch_tickets
+    ticket_ids = (@results['TicketID'] ||
+                  [])[offset_value..(offset_value + max_per_page - 1)] || []
+    if ticket_ids.count == 0
+      @results['tickets'] = []
+      return
+    end
+    response = RestClient::Request
+                 .execute(method: :get,
+                          url: ticket_list_endpoint,
+                          payload: ticket_list_payload(ticket_ids),
+                          headers: {content_type: :json,
+                                    accept: :json})
+    @results['tickets'] = JSON.parse(response).fetch('Ticket', [])
+  end
+
+end
