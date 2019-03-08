9f7874909fd62f6e706b444baf4d78c04b794ad1
Chris Want <cjwant@gmail.com>
2019-03-07 19:55:31 -0700

    Some wordpress styling.

---
+++ app/helpers/wordpress_helper.rb
@@ -0,0 +1,11 @@
+module WordpressHelper
+  def wordpress_age(hit)
+    content_tag(:span,
+                t('views.otrs.age',
+                  age: time_ago_in_words(DateTime.parse(hit['date']))))
+  end
+
+  def wordpress_excerpt(hit)
+    strip_tags(hit['excerpt']['rendered']).html_safe
+  end
+end
