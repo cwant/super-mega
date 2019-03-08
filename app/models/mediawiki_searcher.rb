77f4f36ebb91408530e6ff5ea39db584b3d8fd63
Chris Want <cjwant@gmail.com>
2019-03-08 13:17:15 -0700

    Mediawiki with authentication.

--- app/models/mediawiki_searcher.rb
+++ app/models/mediawiki_searcher.rb
@@ -5,6 +5,40 @@ class MediawikiSearcher < Searcher
   def initialize(config)
     @base_api_url = config[:base_api_url]
     @base_wiki_url = config[:base_wiki_url]
+    auth = config.slice(:username, :password)
+    @auth = if auth.count == 2
+              auth
+            else
+              nil
+            end
+    @token = nil
+    @cookies = nil
+  end
+
+  def mediawiki_fetch_token
+    url = "#{@base_api_url}?action=query&format=json&meta=tokens&type=login"
+    response = RestClient.get(url)
+    return false unless response.code == 200
+
+    @token = JSON.parse(response.body)['query']['tokens']['logintoken']
+    @cookies = response.cookies
+    true
+  end
+
+  def mediawiki_login
+    login_params = {
+      'action' => 'login',
+      'lgname' => @auth[:username],
+      'lgpassword' => @auth[:password],
+      'lgtoken' => @token,
+      'format' => 'json'
+    }
+    response = RestClient.post(@base_api_url, login_params, {cookies: @cookies})
+
+    # Gotta swap out those cookies
+    @cookies = response.cookies
+
+    return (response.code == 200)
   end
 
   def reify
@@ -15,7 +49,16 @@ class MediawikiSearcher < Searcher
     url = "#{@base_api_url}?action=query&list=search&srsearch=#{criteria[:term]}"\
           "&srwhat=text&srlimit=#{max_per_page}&sroffset=#{offset_value}&format=json"
 
-    response = RestClient.get(url)
+    if @auth
+      # I hate you mediawiki
+      # TODO: persist the cookies in session?
+      mediawiki_fetch_token
+      mediawiki_login
+      response = RestClient.get(url, cookies: @cookies)
+    else
+      response = RestClient.get(url)
+    end
+
     @results = JSON.parse(response)
   end
 
